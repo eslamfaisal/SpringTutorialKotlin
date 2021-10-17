a3866beaf4d600996b7e6e0bebfef4bbed71cc47
eslamfaisal <eslam.faisal.ef@gmail.com>
2021-10-17 14:16:21 +0200

    start eureka server application

---
+++ dog-eureka/src/test/java/com/eslam/dogeureka/DogEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eslam.dogeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
